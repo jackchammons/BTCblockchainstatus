cc4a670872f55b3483dbfa82ea968cea9c3be9a2
Ubuntu <jack@Miner.me0i11dwkf3ujffcnpw20gzl3a.bx.internal.cloudapp.net>
2016-07-30 06:51:41 +0000

    fixed the script

---
+++ blockchainstatus.sh
@@ -0,0 +1,1 @@
+echo && while true; do echo $(bitcoin-cli getblockcount)/$(wget -O - http://blockchain.info/q/getblockcount 2>/dev/null)*100 | bc -l | echo -e "\e[1AThe blockchain is $(head -c 6)% synced."; done
